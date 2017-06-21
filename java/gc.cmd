b52af07082f70cb649e281d1fe5e82d9edc72a2f
Jifeng Zhang <zjfroot@gmail.com>
2017-06-21 14:35:27 +0200

    Update gc.cmd

--- java/gc.cmd
+++ java/gc.cmd
@@ -9,3 +9,25 @@ Print heap info
 Dump heap
 
   jmap -dump:file=xxx.jmap <pid>
+
+Minor GC vs Major GC vs Full GC
+
+https://dzone.com/articles/minor-gc-vs-major-gc-vs-full
+
+jstat:
+
+http://docs.oracle.com/javase/7/docs/technotes/tools/share/jstat.html
+
+GC tuning guide:
+
+https://docs.oracle.com/javase/8/docs/technotes/guides/vm/gctuning/sizing.html
+
+CMS collector:
+
+https://stackoverflow.com/questions/19404207/old-gen-heap-is-full-and-the-eden-and-survivor-are-low-and-almost-empty
+https://blog.codecentric.de/en/2013/10/useful-jvm-flags-part-7-cms-collector/
+https://docs.oracle.com/javase/8/docs/technotes/guides/vm/gctuning/cms.html
+
+Java OOM Killer:
+
+https://plumbr.eu/blog/memory-leaks/out-of-memory-kill-process-or-sacrifice-child
